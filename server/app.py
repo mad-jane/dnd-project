7d80955a5eede1d204d83506e7e9b9edba1df077
madeline <madeline.j.dick@gmail.com>
2023-05-18 11:23:49 -0500

    added routes

--- server/app.py
+++ server/app.py
@@ -5,6 +5,44 @@ from sqlalchemy.exc import IntegrityError
 from config import app, db, api
 from models import User, Campaign, CampaignUser, CampaignCharacter, Character
 
+class Campaigns(Resource):
+    
+    def get(self):
+        campaigns = Campaign.query.all()
+        campaigns_dict_list = [campaign.to_dict() for campaign in campaigns]
+        
+        response = make_response(
+            campaigns_dict_list,
+            200
+        )
+        
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        
+        try:
+            campaign = Campaign(
+                title=data['title'],
+                game_master=data['game_master']
+            )
+            
+            db.session.add(campaign)
+            db.session.commit()
+            
+        except Exception as ex:
+            return make_response({
+                'errors': [ex.__str__()]
+            }, 422)
+            
+        response = make_response(
+            campaign.to_dict(),
+            201
+        )
+        
+        return response
+
+api.add_resource(Campaigns, '/campaigns')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
