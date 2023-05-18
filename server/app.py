394bffd5f847bb045ccc23ae9d69f4d7ab330851
madeline <madeline.j.dick@gmail.com>
2023-05-18 13:50:20 -0500

    created CampaignsById class and made a get, patch, and delete request in it

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -43,6 +43,56 @@ class Campaigns(Resource):
         return response
 
 api.add_resource(Campaigns, '/campaigns')
+
+class CampaignsById(Resource):
+    
+    def get(self, id):
+        campaign = Campaign.query.filter_by(id=id).first()
+        
+        if not campaign:
+            return make_response({
+                'error': 'campaign not found.'
+            }, 404)
+            
+        campaign_dict = campaign.to_dict(rules = ('characters',))
+        
+        response = make_response(
+            campaign_dict,
+            200
+        )
+        
+        return response
+    
+    def patch(self, id):
+        campaign = Campaign.query.filter_by(id=id).first()
+        data = request.get_json()
+        
+        for attr in data:
+            setattr(campaign, attr, data[attr])
+            
+        db.session.add(campaign)
+        db.session.commit()
+        
+        response = make_response(
+            campaign.to_dict(),
+            202
+        )
+        
+        return response
+    
+    def delete(self, id):
+        campaign = Campaign.query.filter_by(id=id).first()
+        
+        if not campaign:
+            return make_response({
+                'error': 'campaign not found.'
+            }, 404)
+        
+        db.session.delete(campaign)
+        db.session.commit()
+
+api.add_resource(CampaignsById, '/campaigns<int:id>')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
