0e54a466dc8e94aed069f84489c3a44d21d96460
madeline <madeline.j.dick@gmail.com>
2023-05-23 17:23:08 -0500

    got data to show up

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 // import { Switch, Route } from "react-router-dom";
 
 function App() {
 
-    const [data, setData] = useState([])
+    const [campaigns, setCampaigns] = useState([])
+    const [characters, setCharacters] = useState([])
+    const [users, setUsers] = useState([])
+
+    const fetchCampaignsFallback = useCallback( () => {
+        fetchCampaigns()
+    },
+    [])
+
+    const fetchUsersFallback = useCallback( () => {
+        fetchUsers()
+    },
+    [])
+
+    const fetchCharactersFallback = useCallback( () => {
+        fetchCharacters()
+    },
+    [])
+
 
     useEffect(() => {
+        fetchCampaignsFallback()
+        fetchUsersFallback()
+        fetchCharactersFallback()
+    },[fetchCampaignsFallback, fetchUsersFallback, fetchCharactersFallback])
+
+    const fetchCampaigns = () => {
         fetch("/campaigns").then(
             response => response.json()
         ).then(
             data => {
-                    setData(data)
-                    console.log(data)
+                setCampaigns(data);
             }
+        ).then(
+            console.log("campaigns: ", campaigns)
         )
-    },[])
+    }
+
+    const fetchCharacters = () => {
+        fetch("/characters").then(
+            response => response.json()
+        ).then(
+            data => {
+                setCharacters(data);
+            }
+        ).then(
+            console.log("Characters: ", characters)
+        )
+    }
+
+    const fetchUsers = () => {
+        fetch("/users").then(
+            response => response.json()
+        ).then(
+            data => {
+                setUsers(data);
+            }
+        ).then(
+            console.log("Users: ", users)
+        )
+    }
 
     return (
         <div>
-
+            <h1>{users[0].name}</h1>
+            <h2>{characters[0].name}</h2>
         </div>
     )
 }
